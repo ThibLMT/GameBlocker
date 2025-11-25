2b371c44c4d2924b2c0af8904d4dea15d0af4fb9
ThibLMT <thibaut.lemalet@gmail.com>
2025-11-24 23:57:39 -0500

    Now able to kill process we name

--- GameBlocker/Program.cs
+++ GameBlocker/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 namespace GameBlocker
 {
     internal class Program
@@ -8,15 +9,35 @@ namespace GameBlocker
             //List all running processes
             Process[] processList = Process.GetProcesses();
 
+            List<Process> userApps = new List<Process>();
+
             // We iterate over the array
             foreach (Process p in processList)
             {
                 // We only print if there is a Window Title (to avoid spamming system services)
                 if (!string.IsNullOrEmpty(p.MainWindowTitle))
                 {
+                    userApps.Add(p);
+
                     // The $ allows string interpolation (Like Go's fmt.Printf or JS template literals)
                     Console.WriteLine($"Process: {p.ProcessName} | Title: {p.MainWindowTitle}");
                 }
+            }
+
+            Console.WriteLine("Enter process name to kill: ");
+
+            string target = Console.ReadLine() ?? "";
+
+            Console.WriteLine($"You entered process : {target}");
+
+            foreach (Process process in userApps)
+            {
+                if (String.Equals(process.ProcessName, target, StringComparison.OrdinalIgnoreCase))
+                {
+                    process.Kill();
+                    process.WaitForExit();
+                }
+
             }
 
             // Keep the window open so you can read it (Optional in VS, but good for CLI)
